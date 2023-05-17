8d0dca7f2d83d5144046a7dc722d329e3425c092
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-05-17 12:54:55 +0100

    made  the landing page responsive

--- js/index.js
+++ js/index.js
@@ -18,3 +18,22 @@
   var loadMoreBtn = document.getElementById('loadMoreBtn');
   loadMoreBtn.addEventListener('click', loadMore);
 
+
+  // Function to handle the "Load More" button for section 2
+  function loadMoreSection2() {
+    var section2AdditionalContent = document.getElementById('section2AdditionalContent');
+    var loadMoreBtn2 = document.getElementById('loadMoreBtn2');
+
+    // Toggle the visibility of the additional content section for section 2
+    if (section2AdditionalContent.style.display === 'none') {
+      section2AdditionalContent.style.display = 'block';
+      loadMoreBtn2.textContent = 'Load Less...';
+    } else {
+      section2AdditionalContent.style.display = 'none';
+      loadMoreBtn2.textContent = 'Load More....';
+    }
+  }
+
+  // Attach event listener to the "Load More" button for section 2
+  var loadMoreBtn2 = document.getElementById('loadMoreBtn2');
+  loadMoreBtn2.addEventListener('click', loadMoreSection2);
